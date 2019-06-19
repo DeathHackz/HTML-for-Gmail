ece429efaeca6247c8c0d5768e8edf5b785588ac
DeathHackz <xdeathhackzx@gmail.com>
2019-06-19 11:17:39 -0400

    Created userscript meta file

---
+++ src/HTML-for-Gmail.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          HTML for Gmail
+// @namespace     https://github.com/DeathHackz/HTML-for-Gmail
+// @version       1.0.0
+// @description   Send HTML Formatted Emails With Gmail
+// @match         https://mail.google.com/*
+// @author        DeathHackz
+// @copyright     2019 DeathHackz
+// @license       MIT
+// @homepageURL   https://deathhackz.github.io/HTML-for-Gmail
+// @supportURL    https://github.com/DeathHackz/HTML-for-Gmail/issues
+// @icon          https://github.com/DeathHackz/HTML-for-Gmail/raw/master/icon.png
+// @updateURL     https://raw.githubusercontent.com/DeathHackz/HTML-for-Gmail/master/src/HTML-for-Gmail.meta.js
+// @downloadURL   https://raw.githubusercontent.com/DeathHackz/HTML-for-Gmail/master/src/HTML-for-Gmail.user.js
+// @run-at        document-body
+// ==/UserScript==
